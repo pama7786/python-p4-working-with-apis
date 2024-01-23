1664d94632815cb3f7b02bd755ad11c539701171
abdirahman-abass-diss <abdirahman.diss@student.moringaschool.com>
2024-01-23 13:48:24 +0300

    finished

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -3,12 +3,27 @@ import requests
 import json
 
 class GetPrograms:
+      def program_agencies(self):
+            programs_list = []
+            programs = json.loads(self.get_programs())
+            for program in programs:
+                programs_list.append(program["agency"])
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+            return programs_list
 
-    response = requests.get(URL)
-    return response.content
 
-programs = GetPrograms().get_programs()
-print(programs)
+      def get_programs(self):
+         URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+
+         response = requests.get(URL)
+
+         return response.content
+
+# programs = GetPrograms().get_programs()
+# print(programs)
+
+programs = GetPrograms()
+agencies = programs.program_agencies()
+
+for agency in set(agencies):
+    print(agency)
